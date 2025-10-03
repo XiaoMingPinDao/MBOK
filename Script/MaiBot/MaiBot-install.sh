6597a6a1fc47d76554aa7ff34dc248065aee85b9
zhende1113 <zhende1113@outlook.com>
2025-10-03 22:01:46 +0800

    error: No `pyproject.toml` found in current directory or any parent directory

--- Script/MaiBot/MaiBot-install.sh
+++ Script/MaiBot/MaiBot-install.sh
@@ -328,7 +328,7 @@ install_python_dependencies() {
 
     # 激活虚拟环境
     
-    cd "$DEPLOY_DIR" || exit 1
+    cd "$DEPLOY_DIR/MaiBot" || exit 1
     # 安装 uv 依赖
     export UV_INDEX_URL="https://mirrors.ustc.edu.cn/pypi/simple/"
     mkdir -p ~/.cache/uv
